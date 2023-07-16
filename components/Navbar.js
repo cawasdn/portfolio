f1133ca0cc953f2bfdbe766ed1281b313fc578bf
Daniel <cawasdn@gmail.com>
2023-07-16 15:09:06 +0800

    Added Modal

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,13 @@
 import Link from 'next/link'
-
+import Modal from './Utilities/Modal' //Modal Import
 import { TbExternalLink } from 'react-icons/tb'
 import { GiHamburgerMenu } from 'react-icons/gi'
+import { useState } from 'react'
 
 const Navbar = () => {
+  // Modal Call State
+  const [openModal, setOpenModal] = useState(false)
+
   return (
     <nav className='border-b fixed w-full bg-white z-50 '>
       <div className='container mx-auto flex justify-between items-center h-16  max-w-screen-lg p-4 md:p-0'>
@@ -27,7 +31,14 @@ const Navbar = () => {
               <li>About</li>
             </div>
           </Link>
-          <div className='flex items-center space-x-1 hover:bg-slate-200 px-2 py-1 rounded cursor-pointer'>
+          <div
+            className='flex items-center space-x-1 hover:bg-slate-200 px-2 py-1 rounded cursor-pointer'
+            //  Modal Call Button
+            onClick={() => {
+              setOpenModal(true)
+              document.body.style.overflow = 'hidden'
+            }}
+          >
             <li>Contact</li>
           </div>
           <div className='flex items-center space-x-1 hover:bg-slate-200 px-2 py-1 rounded cursor-pointer'>
@@ -39,6 +50,9 @@ const Navbar = () => {
           <GiHamburgerMenu size='40px' />
         </button>
       </div>
+
+      {/* Modal Call */}
+      {openModal && <Modal closeModal={setOpenModal} />}
     </nav>
   )
 }
--- components/Utilities/Modal.js
+++ components/Utilities/Modal.js
@@ -1,11 +1,14 @@
 import { MdClose } from 'react-icons/md'
 import { SiMinutemailer } from 'react-icons/si'
 
+// md:h-[580px] md:w-[500px]
+// h-full w-full
+
 const modal = ({ closeModal }) => {
   return (
     <div className='modalBackground fixed left-0 right-0 h-screen bottom-0 z-50 bg-slate-300/80 flex justify-center items-center'>
       <div
-        className='modalContainer bg-white h-full w-full p-10 relative rounded-lg shadow-lg md:h-[580px] md:w-[500px]'
+        className='modalContainer bg-white p-0 relative rounded-lg shadow-lg top-[50px] md:top-0'
         data-aos='zoom-in'
         data-aos-delay=''
         data-aos-duration='600'
@@ -22,44 +25,59 @@ const modal = ({ closeModal }) => {
           </button>
         </div>
 
-        <div className='title font-bold text-2xl mb-7'>
-          <h1>Get in Touch</h1>
-        </div>
-        <div className='body flex flex-col gap-3'>
-          <p>Name</p>
-          <input
-            type='text'
-            placeholder='Your Name'
-            required
-            className='w-full bg-slate-100 h-10 p-2'
-          />
-          <p>Email</p>
-          <input
-            type='Email'
-            placeholder='Your Email'
-            required
-            className='w-full bg-slate-100 h-10 p-2'
-          />
-          <p>Message</p>
-          <textarea
-            name=''
-            id=''
-            cols='30'
-            rows='5'
-            placeholder='Your Message'
-            required
-            className='w-full bg-slate-100'
-          ></textarea>
-          <div className='mt-5 p-2 rounded hover:bg-slate-200 transition-all duration-500'>
-            <button className='flex flex-row justify-center items-center w-full gap-2'>
-              <p>Send</p> <SiMinutemailer size={18} />
-            </button>
+        {/* Modal Form */}
+        <div className='m-10 w-[200px] h-[400px] md:w-[500px] md:h-[450px]'>
+          <div className='title font-bold text-2xl mb-7'>
+            <h1>Get in Touch</h1>
           </div>
-          <a href='' className='hover:underline'>
-            <p className='tracking-normal md:tracking-wider'>
-              or use this - decawas@gmail.com
-            </p>
-          </a>
+          <div className='body flex flex-col gap-3'>
+            <div>
+              <p>Name</p>
+              <input
+                type='text'
+                placeholder='Your Name'
+                required
+                className='w-full bg-slate-100 h-10 p-2'
+              />
+            </div>
+            <div>
+              <p>Email</p>
+              <input
+                type='Email'
+                placeholder='Your Email'
+                required
+                className='w-full bg-slate-100 h-10 p-2'
+              />
+            </div>
+            <div>
+              <p>Message</p>
+              <textarea
+                name=''
+                id=''
+                cols='20'
+                rows='3'
+                placeholder='Your Message'
+                required
+                className='w-full bg-slate-100 p-2 resize-none'
+              ></textarea>
+            </div>
+
+            <div className='md:py-2 md:mb-4 md:mt-3 rounded hover:bg-slate-200 transition-all duration-500'>
+              <button className='flex flex-row justify-center items-center w-full gap-2'>
+                <p>Send</p> <SiMinutemailer size={18} />
+              </button>
+            </div>
+            <a href='' className=''>
+              <p className='text-[11px] md:tracking-wider block md:text-[15px]'>
+                or use this -
+                <strong>
+                  <span className='underline p-2 hover:bg-slate-200 rounded transition-all duration-500'>
+                    decawas@gmail.com
+                  </span>
+                </strong>
+              </p>
+            </a>
+          </div>
         </div>
         {/* <div className='footer'>
           <button onClick={() => closeModal(false)}>Cancel</button>
