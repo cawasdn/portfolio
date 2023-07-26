7de1cccc8bc4d8c4fa1a610fae4de0a022cc227e
Daniel <cawasdn@gmail.com>
2023-07-26 12:00:29 +0800

    Portfolio

--- components/About.js
+++ components/About.js
@@ -3,7 +3,7 @@ import AOS from 'aos'
 import 'aos/dist/aos.css'
 import { useEffect } from 'react'
 import Skills from './Skills'
-import Profile from '../public/images/profile.png'
+import Profile from '/profile.png'
 
 const About = () => {
   // useEffect(() => {
